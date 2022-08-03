fb0b51e6e3c5d7a38e2e3a42ebabc4ec289a636c
Yogalakshmi V <yogalakshmi.v@globallogic.com>
2022-08-03 13:10:15 +0530

    product and receipt page added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Setting from './Pages/Setting';
 import Delivery from './Pages/Delivery';
 import CustomerSupport from './Pages/CustomerSupport';
 import CartModule from './Components/NavBar/CartModule';
+import Product from './Pages/Product';
 
 function App() {
  
@@ -22,6 +23,7 @@ function App() {
       <Route path="/delivery" element={<Delivery/>}/>
       <Route path="/CustomerSupport" element={<CustomerSupport/>}/>
       <Route path="/cartModule" element={<CartModule/>}/>
+      <Route path="/product" element={<Product/>}/>
      </Routes>
     </>
   );
